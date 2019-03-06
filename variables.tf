e6767d8fdc6889c336b01e8413408af48230770d
Igor Rodionov <goruha@gmail.com>
2019-03-06 18:46:59 +0600

    Terraform IAM Role module (#1)
    * Added initial version

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name (e.g. `app` or `chamber`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "principals_arns" {
+  type        = "list"
+  description = "List of ARNs to allow assuming the role. Could be AWS services or accounts, Kops nodes, IAM users or groups"
+}
+
+variable "policy_documents" {
+  type        = "list"
+  description = "List of JSON IAM policy documents"
+  default     = []
+}
+
+variable "max_session_duration" {
+  default     = 3600
+  description = "The maximum session duration (in seconds) for the role. Can have a value from 1 hour to 12 hours"
+}
+
+variable "enabled" {
+  type        = "string"
+  description = "Set to `false` to prevent the module from creating any resources"
+  default     = "true"
+}
