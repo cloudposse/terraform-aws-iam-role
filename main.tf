32911f98426451960497cdd1ebe3da13df9eaa35
Maxim Mironenko <mmironenko@chanzuckerberg.com>
2019-05-30 23:00:32 +0600

    update to disable validation

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 module "aggregated_policy" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-iam-policy-document-aggregator.git?ref=tags/0.1.0"
+  source           = "git::https://github.com/cloudposse/terraform-aws-iam-policy-document-aggregator.git?ref=disable-validation"
   source_documents = ["${var.policy_documents}"]
 }
 
