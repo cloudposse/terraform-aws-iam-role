74abfd526e133253f6c3388d73ab41eab45bce8f
Igor Rodionov <goruha@gmail.com>
2019-06-26 21:09:55 +0600

    Use new aggregator

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 module "aggregated_policy" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-iam-policy-document-aggregator.git?ref=tags/0.1.1"
+  source           = "git::https://github.com/cloudposse/terraform-aws-iam-policy-document-aggregator.git?ref=fix-empty-policy"
   source_documents = ["${var.policy_documents}"]
 }
 
