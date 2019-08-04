841f507b4c194dfd7524cb8e39dbbaeb2b28590a
Igor Rodionov <goruha@gmail.com>
2019-08-04 23:21:19 +0600

    Rename key-id to key-arn (#10)

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "kms_key" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=master"
+  source    = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=0.11/master"
   namespace = "cp"
   stage     = "prod"
   name      = "app"
@@ -15,7 +15,7 @@ module "kms_key" {
 }
 
 module "bucket" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-s3-bucket.git?ref=master"
+  source  = "git::https://github.com/cloudposse/terraform-aws-s3-bucket.git?ref=0.11/master"
   enabled = "true"
 
   namespace = "cp"
@@ -29,8 +29,8 @@ module "bucket" {
   versioning_enabled = "false"
   user_enabled       = "false"
 
-  sse_algorithm     = "aws:kms"
-  kms_master_key_id = "${module.kms_key.key_id}"
+  sse_algorithm      = "aws:kms"
+  kms_master_key_arn = "${module.kms_key.key_arn}"
 }
 
 data "aws_iam_policy_document" "resource_full_access" {
