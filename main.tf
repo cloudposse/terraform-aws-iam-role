99e7ac48c24ddf0fad38cf0aff1bc320578c40b8
Jeff <jmw@umbralab.com>
2020-01-15 14:00:11 -0800

    fix

--- main.tf
+++ main.tf
@@ -17,16 +17,8 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = element(keys(var.principals), count.index)
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibility in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
-      identifiers = [var.principals[element(keys(var.principals), count.index)]]
+      type        = element(keys(var.principals), count.index)
+      identifiers = var.principals[element(keys(var.principals), count.index)]
     }
   }
 }
