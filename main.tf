cbb26bce5ed9a788bb7ab54cced9766d08265917
Igor Rodionov <goruha@gmail.com>
2021-03-30 21:10:13 +0600

    Remove dependency from policy-document-aggregator

--- main.tf
+++ main.tf
@@ -12,32 +12,32 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-module "aggregated_assume_policy" {
-  source           = "cloudposse/iam-policy-document-aggregator/aws"
-  version          = "0.8.0"
-  source_documents = data.aws_iam_policy_document.assume_role.*.json
+data "aws_iam_policy_document" "assume_role_aggregated" {
+  count = module.this.enabled ? 1 : 0
+  source_policy_documents = data.aws_iam_policy_document.assume_role.*.json
 }
+
 
 resource "aws_iam_role" "default" {
   count                = module.this.enabled ? 1 : 0
   name                 = var.use_fullname ? module.this.id : module.this.name
-  assume_role_policy   = module.aggregated_assume_policy.result_document
+  assume_role_policy   = join("", data.aws_iam_policy_document.assume_role_aggregated.*.json)
   description          = var.role_description
   max_session_duration = var.max_session_duration
   tags                 = module.this.tags
 }
 
-module "aggregated_policy" {
-  source           = "cloudposse/iam-policy-document-aggregator/aws"
-  version          = "0.8.0"
-  source_documents = var.policy_documents
+data "aws_iam_policy_document" "default" {
+  count                   = module.this.enabled && var.policy_document_count > 0 ? 1 : 0
+  source_policy_documents = var.policy_documents
 }
+
 
 resource "aws_iam_policy" "default" {
   count       = module.this.enabled && var.policy_document_count > 0 ? 1 : 0
   name        = module.this.id
   description = var.policy_description
-  policy      = module.aggregated_policy.result_document
+  policy      = join("", data.aws_iam_policy_document.default.*.json)
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
