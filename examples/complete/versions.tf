9f9e332c6200f54c8c7784291901f66ed1e61a96
nitrocode <nitrocode@users.noreply.github.com>
2022-04-22 07:55:32 -0500

    Bump test modules, correct versions, drop enabled flag

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,10 +4,6 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0, < 4.0"
-    }
-    null = {
-      source  = "hashicorp/null"
       version = ">= 2.0"
     }
   }
