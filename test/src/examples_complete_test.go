e16f18d11a0f4786d84ccf762406e10b9bd365cf
nitrocode <nitrocode@users.noreply.github.com>
2022-04-22 07:32:17 -0500

    Add TestExamplesCompleteDisabled

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -59,3 +59,8 @@ func TestExamplesComplete(t *testing.T) {
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, expectedroleName, roleName)
 }
+
+// Test the Terraform module in examples/complete doesn't attempt to create resources with enabled=false.
+func TestExamplesCompleteDisabled(t *testing.T) {
+	testNoChanges(t, "../../examples/complete")
+}
