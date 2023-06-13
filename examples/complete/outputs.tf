88265be6cbc67c2e75325f88e3e5ac68bd5e93b2
Max Lobur <max_lobur@outlook.com>
2023-06-13 17:11:06 +0300

    upd outputs

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -24,46 +24,46 @@ output "key_arn" {
 }
 
 output "key_id" {
-  value       = coalese(module.kms_key.key_id, null)
+  value       = coalesce(module.kms_key.key_id, null)
   description = "KMS key ID"
 }
 
 output "key_alias_arn" {
-  value       = coalese(module.kms_key.alias_arn, null)
+  value       = coalesce(module.kms_key.alias_arn, null)
   description = "KMS key alias ARN"
 }
 
 output "key_alias_name" {
-  value       = coalese(module.kms_key.alias_name, null)
+  value       = coalesce(module.kms_key.alias_name, null)
   description = "KMS key alias name"
 }
 
 output "bucket_domain_name" {
-  value       = coalese(module.bucket.bucket_domain_name, null)
+  value       = coalesce(module.bucket.bucket_domain_name, null)
   description = "FQDN of bucket"
 }
 
 output "bucket_id" {
-  value       = coalese(module.bucket.bucket_id, null)
+  value       = coalesce(module.bucket.bucket_id, null)
   description = "Bucket Name (aka ID)"
 }
 
 output "bucket_arn" {
-  value       = coalese(module.bucket.bucket_arn, null)
+  value       = coalesce(module.bucket.bucket_arn, null)
   description = "Bucket ARN"
 }
 
 output "user_name" {
-  value       = coalese(module.bucket.user_name, null)
+  value       = coalesce(module.bucket.user_name, null)
   description = "Normalized IAM user name"
 }
 
 output "user_arn" {
-  value       = coalese(module.bucket.user_arn, null)
+  value       = coalesce(module.bucket.user_arn, null)
   description = "The ARN assigned by AWS for the user"
 }
 
 output "user_unique_id" {
-  value       = coalese(module.bucket.user_unique_id, null)
+  value       = coalesce(module.bucket.user_unique_id, null)
   description = "The user unique ID assigned by AWS"
 }
