074386a6ccafd2bdc42e33d3f3fbee57c94b3cb4
Tyrone Meijn <tyrone_meijn@outlook.com>
2026-05-17 14:35:50 +0200

    fix: accept map or list for policy arns

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "name" {
-  value       = join("", aws_iam_role.default.*.name)
+  value       = one(aws_iam_role.default[*].name)
   description = "The name of the IAM role created"
 }
 
 output "id" {
-  value       = join("", aws_iam_role.default.*.unique_id)
+  value       = one(aws_iam_role.default[*].unique_id)
   description = "The stable and unique string identifying the role"
 }
 
 output "arn" {
-  value       = join("", aws_iam_role.default.*.arn)
+  value       = one(aws_iam_role.default[*].arn)
   description = "The Amazon Resource Name (ARN) specifying the role"
 }
 
 output "policy" {
-  value       = join("", data.aws_iam_policy_document.default.*.json)
+  value       = one(data.aws_iam_policy_document.default[*].json)
   description = "Role policy document in json format. Outputs always, independent of `enabled` variable"
 }
 
 output "instance_profile" {
+  value       = one(aws_iam_instance_profile.default[*].name)
   description = "Name of the ec2 profile (if enabled)"
-  value       = join("", aws_iam_instance_profile.default.*.name)
 }
